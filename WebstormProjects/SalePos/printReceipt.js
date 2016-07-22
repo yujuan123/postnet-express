3d992f7220acafd0057946dd3ef3fe2661e5fbf9
yujuan <15809216911@163.com>
2016-07-22 18:12:59 +0800

    modified

---
+++ WebstormProjects/SalePos/printReceipt.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yujuan on 16-7-18.
+ */
+
+var cartItem=[
+    {
+        barcode:"01",
+        name:"可乐",
+        unit:"个" ,
+        price:3
+    },
+    {
+        barcode:"01",
+        name:"可乐",
+        unit:"个" ,
+        price:3
+    },
+
+
+];
+function printReceipt(c)
+{
+
+}
+printReceipt(cartItem);
